1005f0feb940fdbcd1c5239702c6551a4b0d3e74
cedricw <wagner.ced@gmail.com>
2025-11-29 00:40:54 +0100

    add custom filter for multiple fields search

---
+++ src/Filter/MemberSearchFilter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filter;
+
+use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\PropertyInfo\Type;
+
+class GlobalSearchFilter extends AbstractFilter
+{
+    protected function filterProperty(
+        string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?Operation $operation = null, array $context = []
+    ): void {
+        if ($property !== 'search') {
+            return;
+        }
+
+        $alias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder
+            ->andWhere(sprintf(
+                '%1$s.firstname LIKE :search OR %1$s.lastname LIKE :search OR %1$s.email LIKE :search',
+                $alias
+            ))
+            ->setParameter('search', "%$value%");
+    }
+
+    public function getDescription(string $resourceClass): array
+    {
+        return [
+            'search' => [
+                'property' => null,
+                'type' => Type::BUILTIN_TYPE_STRING,
+                'required' => false,
+                'description' => 'Recherche globale sur firstname, lastname, email'
+            ]
+        ];
+    }
+}
